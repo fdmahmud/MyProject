8d3bd395fae849c4e76978c55d58e4c745e8bfc5
Ferdous Mahmud <49809716+fdmahmud@users.noreply.github.com>
2019-04-20 23:01:13 +0600

    I'm exprementing

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MyProject</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<form>
+ 		<div class="form-group row">
+    		<label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    		<div class="col-sm-10">
+     		<input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    		</div>
+  		</div>
+ 		<div class="form-group row">
+    		<label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    		<div class="col-sm-10">
+     			<input type="password" class="form-control" id="inputPassword" placeholder="Password">
+    		</div>
+ 		</div>
+
+ <?php
+
+
+
+?>
+</form>
+</body>
+</html>
